a8133ecaf628a9937bdca7a57244c87b628601c1
Astro?? <andrewsameh911@gmail.com>
2026-09-26 01:23:19 +0300

    Configure editor history unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+    testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
